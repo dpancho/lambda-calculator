735a633a6a383284a3a133ae7feb0707a283937e
Dale Pancho <dalepancho7@gmail.com>
2019-09-11 16:30:03 -0700

    It shows a number.....

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
 
 const Display = (props) => {
+  const [count, setCount] = useState(0);
   return (
   <div className="display">
-    {console.log(props)}
+    <span>{count}</span>
   </div>
   )
 };
